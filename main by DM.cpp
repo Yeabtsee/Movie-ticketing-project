01ae732439778447deb39f79e2932e908dd962ca
Tsion1995 <130348572+Tsion1995@users.noreply.github.com>
2023-08-04 12:15:37 +0300

    Add files via upload

---
+++ main by DM.cpp
@@ -0,0 +1,215 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+const int MAX_MOVIES = 10;
+const int MAX_SEATS = 10;
+
+// Structure to hold movie details
+struct Movie {
+    string title;
+    string showtime;
+    double price;
+};
+
+// Structure to hold seat status
+struct SeatStatus {
+    int movieIndex;
+    int seatNumber;
+    bool isReserved;
+};
+
+// Function to initialize movie data
+void initializeMovies(Movie movies[], int numMovies) {
+    // Add movie data as needed
+    movies[0] = { "Movie 1", "10:00 AM", 10.0 };
+    movies[1] = { "Movie 2", "1:00 PM", 12.0 };
+    movies[2] = { "Movie 3", "4:00 PM", 15.0 };
+}
+
+// Function to display available movies and their details
+void displayMovies(const Movie movies[], int numMovies) {
+    cout << "Available Movies:" << endl;
+    for (int i = 0; i < numMovies; i++) {
+        cout << i + 1 << ". " << movies[i].title << " (" << movies[i].showtime << ")" << endl;
+        cout << "   Price: $" << movies[i].price << endl;
+    }
+}
+
+// Function to read seat reservation status from a file
+void readSeatStatus(SeatStatus seatStatus[][MAX_SEATS], int numMovies) {
+    ifstream inputFile("seat_status.txt");
+    if (inputFile.is_open()) {
+        for (int i = 0; i < numMovies; i++) {
+            for (int j = 0; j < MAX_SEATS; j++) {
+                inputFile >> seatStatus[i][j].isReserved;
+            }
+        }
+        inputFile.close();
+    }
+}
+
+// Function to write seat reservation status to a file
+void writeSeatStatus(const SeatStatus seatStatus[][MAX_SEATS], int numMovies) {
+    ofstream outputFile("seat_status.txt");
+    if (outputFile.is_open()) {
+        for (int i = 0; i < numMovies; i++) {
+            for (int j = 0; j < MAX_SEATS; j++) {
+                outputFile << seatStatus[i][j].isReserved << " ";
+            }
+            outputFile << endl;
+        }
+        outputFile.close();
+    }
+}
+
+// Function to check seat availability for a particular movie and seat number
+bool checkSeatAvailability(const SeatStatus seatStatus[][MAX_SEATS], int movieIndex, int seatNumber) {
+    return !seatStatus[movieIndex][seatNumber].isReserved;
+}
+
+// Function to reserve seats for a particular movie and seat number
+void reserveSeats(SeatStatus seatStatus[][MAX_SEATS], int movieIndex, int seatNumber, int numTickets) {
+    for (int i = seatNumber; i < seatNumber + numTickets; i++) {
+        seatStatus[movieIndex][i].isReserved = true;
+    }
+    cout << "Seats reserved for the movie!" << endl;
+}
+
+// Function to calculate total price based on the number of tickets
+double calculateTotalPrice(double ticketPrice, int numTickets) {
+    return ticketPrice * numTickets;
+}
+
+// Function to calculate total revenue based on the number of tickets reserved for each movie
+double calculateTotalRevenue(const SeatStatus seatStatus[][MAX_SEATS], const Movie movies[], int numMovies) {
+    double totalRevenue = 0.0;
+
+    for (int i = 0; i < numMovies; i++) {
+        int numTicketsSold = 0;
+        for (int j = 0; j < MAX_SEATS; j++) {
+            if (seatStatus[i][j].isReserved) {
+                numTicketsSold++;
+            }
+        }
+        totalRevenue += numTicketsSold * movies[i].price;
+    }
+
+    return totalRevenue;
+}
+
+// Function to reset all seats to available (for managers only)
+void resetAllSeats(SeatStatus seatStatus[][MAX_SEATS], int numMovies) {
+    for (int i = 0; i < numMovies; i++) {
+        for (int j = 0; j < MAX_SEATS; j++) {
+            seatStatus[i][j].isReserved = false;
+        }
+    }
+    cout << "All seats have been reset to available!" << endl;
+}
+
+// Function to authenticate the ticket manager
+bool authenticateManager() {
+    string username, password;
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter password: ";
+    cin >> password;
+
+   
+    return (username == "admin" && password == "password");
+}
+
+// Function to display total revenue for the day
+
+void displayTotalRevenue(const SeatStatus seatStatus[][MAX_SEATS], const Movie movies[], int numMovies) {
+    double totalRevenue = calculateTotalRevenue(seatStatus, movies, numMovies);
+    cout << "Total Revenue: $" << totalRevenue << endl;
+}
+
+int main() {
+    Movie movies[MAX_MOVIES];
+    int numMovies = 3; // Number of available movies
+
+    SeatStatus seatStatus[MAX_MOVIES][MAX_SEATS];
+
+    initializeMovies(movies, numMovies);
+
+    // Load seat reservation status from a file
+    readSeatStatus(seatStatus, numMovies);
+
+    displayMovies(movies, numMovies);
+
+    int userChoice;
+    cout << "What would you like to do?" << endl;
+    cout << "1. Check available movies" << endl;
+    cout << "2. Reserve seats" << endl;
+    cout << "3. Total Revenue (For Ticket Managers Only)" << endl;
+    cout << "4. Reset All Seats (For Ticket Managers Only)" << endl;
+    cout << "Enter your choice: ";
+    cin >> userChoice;
+
+    switch (userChoice) {
+        case 1:
+            displayMovies(movies, numMovies);
+            break;
+        case 2:
+            int movieIndex, seatNumber, numTickets;
+            cout << "Enter the movie number: ";
+            cin >> movieIndex;
+            movieIndex--; // Adjusting to 0-based index
+
+            if (movieIndex >= 0 && movieIndex < numMovies) {
+                cout << "Enter the first seat number (1-" << MAX_SEATS << "): ";
+                cin >> seatNumber;
+                seatNumber--; // Adjusting to 0-based index
+
+                if (seatNumber >= 0 && seatNumber < MAX_SEATS) {
+                    cout << "Enter the number of tickets to reserve: ";
+                    cin >> numTickets;
+
+                    if (seatNumber + numTickets <= MAX_SEATS &&
+                        checkSeatAvailability(seatStatus, movieIndex, seatNumber)) {
+                        double totalPrice = calculateTotalPrice(movies[movieIndex].price, numTickets);
+                        cout << "Total price: $" << totalPrice << endl;
+
+                        reserveSeats(seatStatus, movieIndex, seatNumber, numTickets);
+
+                        // Save seat reservation status to a file
+                        writeSeatStatus(seatStatus, numMovies);
+                    } else {
+                        cout << "Invalid seat number or the seats are already reserved." << endl;
+                    }
+                } else {
+                    cout << "Invalid seat number!" << endl;
+                }
+            } else {
+                cout << "Invalid movie number!" << endl;
+            }
+            break;
+        case 3:
+            // Ticket manager authentication
+            if (authenticateManager()) {
+                displayTotalRevenue(seatStatus, movies, numMovies);
+            } else {
+                cout << "Authentication failed. Only ticket managers can access this option." << endl;
+            }
+            break;
+        case 4:
+            // Ticket manager authentication
+            if (authenticateManager()) {
+                resetAllSeats(seatStatus, numMovies);
+                // Save seat reservation status to a file after resetting
+                writeSeatStatus(seatStatus, numMovies);
+            } else {
+                cout << "Authentication failed. Only ticket managers can access this option." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid choice!" << endl;
+    }
+
+    return 0;
+}
